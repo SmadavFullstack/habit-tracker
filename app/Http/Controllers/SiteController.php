c5e7d0c7c11a30d8480b764d0b7db463a1b842be
smadav999 <alfredodomingos804@gmail.com>
2026-08-13 22:28:30 +0100

    first commit

---
+++ app/Http/Controllers/SiteController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Controllers;
+
+class SiteController extends Controller
+{
+    public function index()
+    {
+
+        $nome = "Alfredo";
+        $habitos = [
+            'Acordar cedo',
+            'Exercícios físicos',
+            'Meditação',
+            'Leitura diária',
+        ];
+        return view('home', compact('nome', 'habitos'));
+    }
+}
---
+++ resources/views/home.blade.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Habit Manager</title>
+</head>
+
+<body>
+    <h1>
+        Welcome, to the Habit Manager!
+    </h1>
+    <hr>
+    <p>Olá, {{ $nome }}! Aqui estão seus hábitos:</p>
+    <ul>
+        @foreach ($habitos as $habito)
+            <li>{{ $habito }}</li>
+        @endforeach
+    </ul>
+
+</body>
+
+</html>

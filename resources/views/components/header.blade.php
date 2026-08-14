594c43361ad5b07a2572803d9f6131ab4d9200c6
SmadavFullstack <alfredodomingos804@gmail.com>
2026-08-14 20:58:06 +0100

    Novo

--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -1,11 +1,11 @@
 <header class="bg-white border-b-2 flex items-center justify-between p-4!">
 
-    <div class="bg-amber-600 text-white w-37.5 p-3.75! shadow-[0_1px_4px_rgb(0,0,0)] text-center">LOGO</div>
+    <div class="bg-amber-600 text-white w-37.5 p-3.75! shadow-[0_1px_4px_rgb(0,0,0)] text-center rounded">LOGO</div>
     <nav class="w-110 flex flex-row justify-around items-center">
-        <ul class="w-full p-3.75! flex flex-row justify-around items-center">
+        <ul class="w-full flex flex-row justify-between items-center">
             <li class="cursor-pointer"><a href="" class="text-amber-600">Entrar</a></li>
             <li class="cursor-pointer"><a href="" class="text-amber-600">Cadastrar</a></li>
-            <li class="bg-amber-600 text-white w-37.5 p-3.75! shadow-[0_1px_4px_rgb(0,0,0)] text-center cursor-pointer">
+            <li class="bg-amber-600 text-white w-37.5 p-3.75! shadow-[0_1px_4px_rgb(0,0,0)] text-center cursor-pointer rounded">
                 <a href="https://github.com/SmadavFullstack/habit-tracker" target="_blank">Github</a>
             </li>
         </ul>
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,11 +1,30 @@
 <x-layout>
 
-    <section>
+    <section class="flex flex-col items-start justify-center p-20! h-[80vh] gap-10">
         <header>
-            <h1>HABIT TRACKER</h1>
+            <h1 class="text-4xl font-bold">HABIT TRACKER</h1>
         </header>
-        <P>Crie hábitos construtivos de forma mais precisa com apenas um clique!</P>
-        <button>Criar</button>
+        <P class="text-justify indent-7.5 w-[80vw]">
+            Crie hábitos construtivos de forma mais precisa com apenas um clique!
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Aut, architecto tempore. Velit ipsam accusantium
+            ipsa, recusandae quos, officiis ipsum esse dolores a soluta magnam officia iusto sed doloremque placeat
+            quia?Quibusdam ad saepe quod rem consequatur nihil eos soluta repudiandae suscipit tenetur aliquid, nesciunt
+            deserunt ipsum! Itaque ipsam accusantium et? Voluptates libero fugit quaerat neque temporibus in, suscipit
+            atque necessitatibus.
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus, cumque assumenda totam a in minima
+            sapiente numquam molestiae, distinctio cum officia voluptatem accusamus tempore, repellendus eaque obcaecati
+            laboriosam itaque quod?
+            Ab at consequuntur provident ea suscipit reiciendis qui doloribus voluptate a. Sed, incidunt distinctio
+            repudiandae sunt nobis labore autem earum vero, explicabo laborum eaque natus quod delectus. Totam, a
+            expedita!
+            Assumenda architecto iure provident libero, maiores fugit quisquam deserunt est perspiciatis consequatur
+            cumque nisi iste ratione eaque eveniet rerum a nostrum vero totam consectetur labore numquam accusantium
+            debitis aspernatur! Quibusdam?
+            Quos fugiat ab perspiciatis illum iure saepe a, dicta voluptates nisi eius, rem facere delectus dolor error
+            mollitia illo quibusdam aut aliquam et. Saepe dolorum quos aperiam fugiat, reprehenderit odio!
+        </P>
+        <button
+            class="h-12.5 w-37.5 bg-amber-600 text-white font-sans font-bold cursor-pointer shadow-[0_1px_4px_rgb(0,0,0)] rounded">Criar</button>
 
     </section>
 

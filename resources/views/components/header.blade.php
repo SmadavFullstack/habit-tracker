3d11b310e7b1f668c5c24cccf79aa8cc24eee089
SmadavFullstack <alfredodomingos804@gmail.com>
2026-08-14 16:49:26 +0100

    Novo

--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -1,7 +1,12 @@
 <header class="bg-white border-b-2 flex items-center justify-between p-4!">
 
-<div class="bg-amber-600 text-white w-37.5 p-3.75! shadow-[0_1px_4px_rgb(0,0,0)] text-center">LOGO</div>
-
-<div class="bg-amber-600 text-white w-37.5 p-3.75! shadow-[0_1px_4px_rgb(0,0,0)] text-center">Github</div>
+    <div class="bg-amber-600 text-white w-37.5 p-3.75! shadow-[0_1px_4px_rgb(0,0,0)] text-center">LOGO</div>
+    <nav class="w-100">
+        <ul class="w-full p-3.75! flex flex-row justify-around items-center">
+            <li class="cursor-pointer"><a href="">Entrar</a></li>
+            <li class="cursor-pointer"><a href="">Cadastrar</a></li>
+        </ul>
+    </nav>
+    <div class="bg-amber-600 text-white w-37.5 p-3.75! shadow-[0_1px_4px_rgb(0,0,0)] text-center">Github</div>
 
 </header>

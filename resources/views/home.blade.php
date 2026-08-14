98df7ef8b16381b7d5d1aa8b3f7bf7abf08a7820
SmadavFullstack <alfredodomingos804@gmail.com>
2026-08-14 17:14:28 +0100

    Novo

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,5 +1,12 @@
 <x-layout>
 
-    <h1>Bem-vindo ao Gerenciador de Hábitos!</h1>
+    <section>
+        <header>
+            <h1>HABIT TRACKER</h1>
+        </header>
+        <P>Crie hábitos construtivos de forma mais precisa com apenas um clique!</P>
+        <button>Criar</button>
+
+    </section>
 
 </x-layout>

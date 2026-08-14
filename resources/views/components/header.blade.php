44a5a2a156540d3366b4a312b8391b76b63f3d61
SmadavFullstack <alfredodomingos804@gmail.com>
2026-08-14 17:05:53 +0100

    Novo

--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -1,12 +1,14 @@
 <header class="bg-white border-b-2 flex items-center justify-between p-4!">
 
     <div class="bg-amber-600 text-white w-37.5 p-3.75! shadow-[0_1px_4px_rgb(0,0,0)] text-center">LOGO</div>
-    <nav class="w-100">
+    <nav class="w-110 flex flex-row justify-around items-center">
         <ul class="w-full p-3.75! flex flex-row justify-around items-center">
-            <li class="cursor-pointer"><a href="">Entrar</a></li>
-            <li class="cursor-pointer"><a href="">Cadastrar</a></li>
+            <li class="cursor-pointer"><a href="" class="text-amber-600">Entrar</a></li>
+            <li class="cursor-pointer"><a href="" class="text-amber-600">Cadastrar</a></li>
+            <li class="bg-amber-600 text-white w-37.5 p-3.75! shadow-[0_1px_4px_rgb(0,0,0)] text-center cursor-pointer">
+                <a href="https://github.com/SmadavFullstack/habit-tracker" target="_blank">Github</a>
+            </li>
         </ul>
     </nav>
-    <div class="bg-amber-600 text-white w-37.5 p-3.75! shadow-[0_1px_4px_rgb(0,0,0)] text-center">Github</div>
 
 </header>

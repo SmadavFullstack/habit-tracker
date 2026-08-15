bdccac8069e3eb77d7a26e704ac929ede9520e8e
SmadavFullstack <alfredodomingos804@gmail.com>
2026-08-15 02:46:58 +0100

    feat(Login)

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,9 @@
 <?php
 
 use App\Http\Controllers\SiteController;
+use App\Http\Controllers\Auth\siteLogin;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', [SiteController::class, 'index']);
+
+Route::get('/login', [siteLogin::class, 'index']);

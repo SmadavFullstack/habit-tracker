6bf60f2a057d991d7828a7499cecfcbe649af73b
SmadavFullstack <alfredodomingos804@gmail.com>
2026-08-19 19:44:37 +0100

    (offline)

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -5,10 +5,10 @@
         <h1 class="text-3xl w-full text-center">Login</h1>
 
         <form action="/login" method="post"
-            class="bg-white h-79 w-130 rounded flex flex-col justify-evenly items-center shadow-md">
+            class="bg-white h-79 w-130 rounded flex flex-col justify-evenly items-center shadow-md p-4!">
             @csrf
             @error('email')
-                <p class="text-red-500 text-sm">{{ $message }}</p>
+                <p class="text-red-500 text-sm border-2 rounded p-1!">{{ $message }}</p>
             @enderror
             <div class="flex flex-col gap-2 p-4! rounded w-[90%]">
                 <label for="email">Email</label>
